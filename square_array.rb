4b2a5a64785a2bb06b10a16d33863d9d0e7ffbfd
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-11-19 18:31:03 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
-def square_array(array)
-  # your code here
-  squared = [];
-  array.each do |element|
-    squared.push(element**2)
-  end 
-  squared
-end
+# def square_array(array)
+#   # your code here
+#   squared = [];
+#   array.each do |element|
+#     squared.push(element**2)
+#   end 
+#   squared
+# end
 
 def square_array_collected 
-  array.collect{}
+  array.collect{|element| element**2}
 end
